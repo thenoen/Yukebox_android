9c666be43988938f6d67d8f8d7582da5f30d6eab
marek <kordikmarek@gmail.com>
2017-03-23 21:56:32 +0100

    started implementation of communication between client and server

--- yukebox-client/src/app/app.component.ts
+++ yukebox-client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  apiServerResponse;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    console.log("going to make request");
+    this.apiService.getStringResponse().subscribe();
+    console.log("making request finished");
+  }
+
 }
--- yukebox-client/src/app/app.module.ts
+++ yukebox-client/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { ApiService } from './services/api.service';
+
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
