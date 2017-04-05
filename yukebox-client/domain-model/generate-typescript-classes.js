c43f3b41594c60740a76a4e94f07b12d852cc856
marek <kordikmarek@gmail.com>
2017-04-05 23:32:21 +0200

    fixed cleaning of old typescript classes before generating of new

--- yukebox-client/domain-model/generate-typescript-classes.js
+++ yukebox-client/domain-model/generate-typescript-classes.js
@@ -1,7 +1,12 @@
 const jsonToTypescript = require('json-schema-typescript');
+var rimraf = require('rimraf');
 
 console.log(__dirname);
 
-jsonToTypescript(`${__dirname}/schemas/**/*.json`, `${__dirname}/../src/app/domain`)
+var outputPath = `${__dirname}/../src/app/domain`;
+
+rimraf.sync(outputPath, {}, function () { console.log('done'); });
+
+jsonToTypescript(`${__dirname}/schemas/**/*.json`, outputPath)
     .then(files => console.log(files.length, 'TypeScript interfaces generated'))
     .catch(ex => console.error(ex));
