e60db7da3129a6761f6970b3a1b40003a3977903
marek <kordikmarek@gmail.com>
2017-05-05 23:20:46 +0200

    updated to angular 4; added ng-bootstrap test tabs component

--- yukebox-client/src/app/app.module.ts
+++ yukebox-client/src/app/app.module.ts
@@ -2,24 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { ApiService } from './services/api.service';
-
-
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { NgbdTabsetConfig } from './tabset/tabset.component'
 import { VideoResult } from './video-result/video-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFormComponent,
+    NgbdTabsetConfig,
     VideoResult
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    NgbModule.forRoot()
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
