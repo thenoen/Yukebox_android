9e07e3b6ffa30891d95d174193c92ddedf8b4eed
marek <kordikmarek@gmail.com>
2017-11-09 21:57:44 +0100

    updated gradle, useless code cleanup

--- app/src/main/java/sk/thenoen/yukebox/httpserver/controller/SearchController.java
+++ app/src/main/java/sk/thenoen/yukebox/httpserver/controller/SearchController.java
@@ -31,25 +31,6 @@ public class SearchController extends RouterNanoHTTPD.GeneralHandler {
 
 	@Override
 	public NanoHTTPD.Response get(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
-		StringBuilder text = new StringBuilder("<html><body>");
-		text.append("<h1>Url: ");
-		text.append(session.getUri());
-		text.append("</h1><br>");
-		Map<String, String> queryParams = session.getParms();
-		if (queryParams.size() > 0) {
-			for (Map.Entry<String, String> entry : queryParams.entrySet()) {
-				String key = entry.getKey();
-				String value = entry.getValue();
-				text.append("<p>Param '");
-				text.append(key);
-				text.append("' = ");
-				text.append(value);
-				text.append("</p>");
-			}
-		} else {
-			text.append("<p>no params in url</p><br>");
-		}
-
 		YoutubeService youtubeService = YoutubeService.getInstance();
 		List<SearchResult> searchResults = youtubeService.search(session.getParameters().get("query").get(0));
 
