c96a82a4ef8dc28eb9f86764d000d68324e4a4d9
mkordik <mkordik@davincisoftware.sk>
2017-12-31 13:46:13 +0100

    attempts to play video without interruption during screen rotation

--- app/src/main/java/sk/thenoen/yukebox/activity/PlayerActivity.java
+++ app/src/main/java/sk/thenoen/yukebox/activity/PlayerActivity.java
@@ -3,9 +3,16 @@ package sk.thenoen.yukebox.activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.transition.TransitionManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,6 +46,9 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
 
 	private Handler guiUpdateHandler = new Handler();
 
+	@BindView(android.R.id.content)
+	View rootActivityView;
+
 	@BindView(R.id.log_text)
 	TextView statusText;
 
@@ -51,6 +61,9 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
 	@Inject
 	VideoDao videoDao;
 
+	private MediaPlayer mediaPlayer;
+	private int time = -1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		AndroidInjection.inject(this);
@@ -62,6 +75,35 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
 		startHttpServer(wwwDirectory);
 		displayServerEndpoint();
 		initYoutubePlayer();
+
+		if (savedInstanceState != null) {
+			time = savedInstanceState.getInt("time", -1);
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle bundle) {
+		super.onSaveInstanceState(bundle);
+
+		int currentTimeMillis = mediaPlayer.getCurrentTimeMillis();
+		bundle.putInt("time", currentTimeMillis);
+
+		Log.i("my_tag", "time - activity: " + currentTimeMillis);
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		rotateView();
+	}
+
+	private void rotateView() {
+		ConstraintSet constraintSet = new ConstraintSet();
+		constraintSet.load(this, R.layout.player_activity_layout_land);
+
+		ConstraintLayout constraintLayout = (ConstraintLayout) ((FrameLayout) rootActivityView).getChildAt(0);
+		TransitionManager.beginDelayedTransition(constraintLayout);
+		constraintSet.applyTo(constraintLayout);
 	}
 
 	private void displayServerEndpoint() {
@@ -98,9 +140,41 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
 
 	@Override
 	public void onInitializationSuccess(YouTubePlayer.Provider provider, final YouTubePlayer youTubePlayer, boolean b) {
-		MediaPlayer mediaPlayer = new MediaPlayer(youTubePlayer);
+		mediaPlayer = new MediaPlayer(youTubePlayer);
+		youTubePlayer.setPlaybackEventListener(new YouTubePlayer.PlaybackEventListener() {
+
+			@Override
+			public void onPlaying() {
+
+			}
+
+			@Override
+			public void onPaused() {
+				Log.i("my_tag", "time - handler pause: " + youTubePlayer.getCurrentTimeMillis());
+			}
+
+			@Override
+			public void onStopped() {
+				Log.i("my_tag", "time - handler stop: " + youTubePlayer.getCurrentTimeMillis());
+			}
+
+			@Override
+			public void onBuffering(boolean b) {
+
+			}
+
+			@Override
+			public void onSeekTo(int i) {
+
+			}
+		});
 		apiHttpServer.addMapping(MediaPlayerController.ROUTE_MAPPING, MediaPlayerController.ROUTE_PRIORITY, MediaPlayerController.class, mediaPlayer);
 		guiUpdateHandler.post(() -> updateStatusText(getResources().getString(R.string.server_initialization_status_ok)));
+		if (time != -1) {
+			Log.i("my_tag", "play time: " + youTubePlayer.getCurrentTimeMillis());
+			youTubePlayer.play();
+			Log.i("my_tag", "play time2: " + youTubePlayer.getCurrentTimeMillis());
+		}
 	}
 
 	@Override
@@ -125,5 +199,7 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
 
 		Toast toast = Toast.makeText(this, getText(R.string.url_copied_toast), Toast.LENGTH_LONG);
 		toast.show();
+
+		rotateView();
 	}
 }
--- app/src/main/java/sk/thenoen/yukebox/service/MediaPlayer.java
+++ app/src/main/java/sk/thenoen/yukebox/service/MediaPlayer.java
@@ -46,4 +46,8 @@ public class MediaPlayer {
 		);
 		youTubePlayer.cueVideo(videoId);
 	}
+
+	public int getCurrentTimeMillis() {
+		return youTubePlayer.getCurrentTimeMillis();
+	}
 }
